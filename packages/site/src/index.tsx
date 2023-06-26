a069cb20c041d4b458e6b52ce2f34684d20b3942
lx70770 <lx70770@lay2.dev>
2023-06-26 18:24:26 +0800

    feat: add react GA

--- packages/site/src/index.tsx
+++ packages/site/src/index.tsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client'
 import { RecoilRoot } from 'recoil'
+import ReactGA from 'react-ga'
 import { Toaster } from '@/components'
 import { ThemeProvider, ErrorBoundary } from '@/context'
 import reportWebVitals from '@/reportWebVitals'
 import App from './App'
 import '@/assets/styles/index.scss'
+
+ReactGA.initialize('UA-000000-00', { debug: true })
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<RecoilRoot>

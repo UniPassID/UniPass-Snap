bd4b9f9751fabfd0e3b86e6db1ad24917625839b
lx70770 <lx70770@lay2.dev>
2023-06-26 11:27:35 +0800

    fix: refactor

--- packages/site/src/router/index.ts
+++ packages/site/src/router/index.ts
@@ -2,7 +2,6 @@ import { createBrowserRouter, RouteObject } from 'react-router-dom'
 import PageNotFound from '@/pages/404'
 import Home from '@/pages/home'
 import Demo from '@/pages/demo'
-import Pay from '@/pages/pay'
 
 const routes: RouteObject[] = [
 	{
@@ -12,10 +11,6 @@ const routes: RouteObject[] = [
 	{
 		path: '/demo',
 		Component: Demo
-	},
-	{
-		path: '/pay',
-		Component: Pay
 	},
 	{
 		path: '/*',
